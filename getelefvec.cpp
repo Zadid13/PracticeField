bef466e7adab82a78c9db806557c8d8d55000042
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-26 23:27:16 +0600

    Add files via upload

---
+++ getelefvec.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    vector<int> g1{1,2,3,4,5};
+
+    cout<<"Element at index 0:  "<<g1.at(0)<<endl;
+    cout<<"Element at index 2:  "<<g1.at(2)<<endl;
+    cout<<"Element at index 4:  "<<g1.at(4)<<endl;
+
+    return 0;
+}
