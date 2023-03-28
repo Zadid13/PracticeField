5c7716fb64f2d7d3548282308e40485ceb28727c
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-29 01:00:48 +0600

    Add files via upload

---
+++ specificsortwvec.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    vector<int>v= {5,3,1,2};
+
+
+    sort(v.begin(),v.begin()+3);
+
+    for(auto u:v)cout<<u<<" ";
+
+    cout<<endl;
+
+
+}
