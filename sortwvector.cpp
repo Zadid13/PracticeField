cf4fe684dfd283149aa740fe88a33e87f9a60962
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-29 00:55:10 +0600

    Add files via upload

---
+++ sortwvector.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    vector<int>v= {5,3,1,2};
+
+
+    sort(v.begin(),v.end());
+
+    for(auto u:v)cout<<u<<" ";
+
+    cout<<endl;
+
+
+}
