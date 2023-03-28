b5421484a5cbd17443e3eb5a2cb03c8b840ff3b4
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-29 01:54:38 +0600

    Add files via upload

---
+++ dellastelewvec.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    vector<int>v= {2,3,4,5};
+
+    cout<<v.back()<<endl;//O(1);
+    v.pop_back();
+
+    cout<<v.back()<<endl;
+
+}
