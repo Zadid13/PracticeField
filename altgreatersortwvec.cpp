78567a7ab2c40bcf8b76f0e8900cd53f9ffa3959
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-29 01:22:24 +0600

    Add files via upload

---
+++ altgreatersortwvec.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    vector<int>v= {5,3,4,7,9,3,1,2};
+
+    sort(v.rbegin(),v.rend());
+
+    for(auto u:v)cout<<u<<" ";
+
+    cout<<endl;
+
+
+}
