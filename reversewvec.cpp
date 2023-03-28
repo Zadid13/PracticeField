9b291859a1af6b6efc23d683ed17b7c901a210cd
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-03-29 01:34:32 +0600

    Add files via upload

---
+++ reversewvec.cpp
@@ -0,0 +1,17 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+
+{
+
+    vector<int>v= {5,3,1,4};//4 1 3 5
+
+    reverse(v.begin(),v.end());
+
+    for(auto u:v)cout<<u<<" ";
+
+    cout<<endl;
+
+
+}
