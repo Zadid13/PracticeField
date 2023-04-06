002c231a1ab39dae0a85371c9762216ad1cb7646
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-06 06:06:15 +0600

    Add files via upload

---
+++ BaseStr.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s;
+
+    s = s + 'a'; // s="a"
+    s = s + 'b'; // s="ab"
+    s += 'c'; //s = "abc"
+
+    cout<<s<<endl;
+
+}
