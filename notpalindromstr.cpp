9a7f9a13cdcca41a3e184ba4adf6dfab905565c8
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-08 07:48:58 +0600

    Add files via upload

---
+++ notpalindromstr.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "aabAa";
+    string tmp= s1;
+
+    reverse(tmp.begin(),tmp.end());
+
+    if(s1==tmp)cout<<"Palindrom\n";
+    else cout<<"Not Palindrom\n";
+
+}
