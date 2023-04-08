58410b55716efdd323cb04e99805d68be4e7c2e5
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-08 07:09:47 +0600

    Add files via upload

---
+++ iteratorwstr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hasnat Zadid Shanto";
+
+    string::iterator it;
+    for(it=s.begin(); it!=s.end(); it++)
+    {
+        cout<<*it;
+    }
+
+    cout<<endl;
+
+
+}
