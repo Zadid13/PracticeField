20ab73a9e52b8ed2f075720c6e6d6ce430e07ffd
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-08 06:51:59 +0600

    Add files via upload

---
+++ constr.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    string s1= "Hasnat",s2 ="Zadid";
+
+    char c=  ' ';
+    s1+=c;
+    s1+=s2;
+
+    cout<<s1<<endl;
+
+
+}
