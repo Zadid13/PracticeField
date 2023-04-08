7a5d1994ec381fd717a3526ab1f4b265f553c547
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-08 08:08:34 +0600

    Add files via upload

---
+++ fastio.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    optimize();
+
+    string s;
+    cin>>s;
+
+    cout<<s<<endl;
+
+
+}
