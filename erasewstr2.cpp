f81ca5da8d48042563ded42970afe8987e555496
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-14 11:00:13 +0600

    Add files via upload

---
+++ erasewstr2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+int main(){
+
+    optimize();
+
+    string s = "deacbAAA";
+
+    s.erase(s.end()-1); //O(n)
+    cout<<s<<endl;
+
+}
