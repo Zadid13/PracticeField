4eb7d7f01a1f67194df4338b1172d8a6ab749656
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-19 15:52:34 +0600

    Add files via upload

---
+++ strwvec2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+
+    optimize();
+    string s = "eee",t="ee";
+
+    if(s<t) cout<<s<<" "<<"is smallest";
+    else cout<<t<<" "<<"is smallest";
+
+
+}
