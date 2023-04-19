8249f8c044ba64ff69f1cab8ae147539a7254827
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-04-19 15:48:35 +0600

    Add files via upload

---
+++ strwvec.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+
+    optimize();
+    string s = "eeemon",t="eemomo";
+
+    if(s<t) cout<<s<<" "<<"is smallest";
+    else cout<<t<<" "<<"is smallest";
+
+
+
+
+   /* vector<string> v;
+    v.psuh_back("Zadid");
+    v.push_back("Hasnat");
+    v.push_back("shanto");
+    v.push_back("Hasan");
+    v.push_back("talukder");
+    v.push_back("Moni");*/
+
+
+}
