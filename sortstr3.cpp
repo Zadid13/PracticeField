f7afcd580633dcd39bce722d8f80dbda36c31886
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-05-23 23:16:03 +0600

    Add files via upload

---
+++ sortstr3.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+
+    optimize();
+
+    vector<string> v;
+
+    v.push_back("Shahriar");
+    v.push_back("Mubarrat");
+    v.push_back("emon");
+    v.push_back("Nishan");
+    v.push_back("momo");
+    v.push_back("Prity");
+    v.push_back("Momo");
+    v.push_back("AAAA");
+    v.push_back("AA");
+
+    sort(v.begin(),v.end());
+
+    for(auto u:v) cout<<u<<endl;
+
+
+
+}
