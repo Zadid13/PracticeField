bc76a4f503689e655fe080337b2c10cec6d57f9e
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-05-27 22:55:47 +0600

    Add files via upload

---
+++ checkspace.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+int main()
+{
+    optimize();
+
+    char c = ' ';
+
+    bool f1 = isspace(c);
+
+    cout << f1 << endl;
+
+    return 0;
+}
