53a9c4ecd254517ac3ba5135ece966e8eca8665a
Talukder Hasnat Zadid <77864700+Zadid13@users.noreply.github.com>
2023-05-27 23:05:17 +0600

    Add files via upload

---
+++ toupcase.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+
+    char c = 'b';
+
+    c = toupper(c);
+    cout << c << endl;
+
+    return 0;
+}
